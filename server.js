48dfe9255005b3c2cb01c177e1e774c16658b21d
Hundaol Negusie <hundaolnk@gmail.com>
2022-11-24 15:59:09 +0300

    added dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const path = require("path");
+// const path = require("path");
 const cors = require("cors");
 const db = require("./config/dbConn");
 
